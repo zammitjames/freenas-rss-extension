8621fdb66e81843fc82ba334462826ce98d75cce
tresni <tresni@d4fb0142-069c-11de-ba4d-252e5d5a8945>
2009-07-03 04:40:49 +0000

    Branching for cookie feature

---
+++ branches/cookie/sys/rss_cron.php
@@ -0,0 +1,133 @@
+#!/usr/local/bin/php
+<?php
+require_once('ext/RSS/rss_class_history.php');
+require_once('ext/RSS/rss_functions.inc');
+require_once('XML/Unserializer.php');
+
+function get_download($item) {
+    if (isset($item['enclosure']) && isset($item['enclosure']['attributes'])  && $item['enclosure']['attributes']['type'] == 'application/x-bittorrent')
+        return $item['enclosure']['attributes']['url'];
+    return $item['link'];
+}
+
+function get_guid($item) {
+    if (!isset($item['guid']) || empty($item['guid']))
+        return get_download($item);
+
+    if (is_array($item['guid']))
+        return $item['guid']['_content'];
+    return $item['guid'];
+}
+
+function add_item($feed, $item) {
+    global $History;
+    
+    $History->add($feed['uuid'], array(
+        'title' => $item['title'],
+        'guid' => get_guid($item),
+        'description' => $item['description'],
+        'pubDate' => $item['pubDate'],
+        'link' => get_download($item),
+        'downloaded' => (isset($item['downloaded']) ? true : false),
+        'filter' => (isset($item['filter']) ? $item['filter'] : false)
+    ));
+}
+
+// We don't have any feeds, just exit.  Filters are not required.
+if (!isset($config['rss']) || !isset($config['rss']['feeds']) || !isset($config['rss']['feeds']['rule'])) {
+    rss_log('No valid configuration', VERBOSE_ERROR);
+    exit();
+}
+
+$a_feeds = &$config['rss']['feeds']['rule'];
+if (isset($config['rss']) && isset($config['rss']['filters']))
+    $a_filters = &$config['rss']['filters']['rule'];
+else
+    $a_filters = array();
+
+$History = new History($config['rss']);
+$History->read();
+
+$options = array(
+    'parseAttributes' => TRUE,
+    'attributesArray' => 'attributes'
+);
+$Unserializer = &new XML_Unserializer($options);
+
+foreach ($a_feeds as &$feed) {
+    if(!isset($feed['enabled'])) continue;
+
+    rss_log("Getting feed {$feed['name']}", VERBOSE_EXTRA);
+    
+    $status = $Unserializer->unserialize($feed['_url'], true);
+    if (PEAR::isError($status)) die($status->getMessage());
+    
+    $data = $Unserializer->getUnserializedData();
+    if ($data == false) {
+        rss_log("Unable to unserialize {$feed['name']}", VERBOSE_EXTRA);
+        continue;
+    }
+    
+    if (!isset($data['channel']['item'])) {
+        rss_log("No item data", VERBOSE_EXTRA);
+        continue;
+    }
+    
+    if (!isset($data['channel']['item'][0]))
+      $data['channel']['item'] = array($data['channel']['item']);
+
+    foreach ($data['channel']['item'] as $item) {
+        if (!is_array($item)) {
+            rss_log("Invalid feed data for {$feed['name']}", VERBOSE_EXTRA);
+            continue;
+        }
+        
+        if ($History->find($feed['uuid'], get_guid($item))) { rss_log("{$item['title']} found", VERBOSE_EXTRA); continue; }
+
+        if (isset($feed['subscribe'])) {
+            if (add_torrent(get_download($item), $feed['directory']) == 0) $item['downloaded'] = true;
+        } else {
+            foreach ($a_filters as &$filter) {
+                if (!isset($filter['enabled'])) continue;
+                if ($filter['feed'] != -1 && $filter['feed'] != $feed['uuid']) continue;
+                
+                if (preg_match('/'.$filter['filter'].'/i', $item['title']))
+                {
+                    rss_log("{$item['title']} matches {$filter['filter']}", VERBOSE_EXTRA);
+                    $item['filter'] = $filter['uuid'];
+
+                    if (isset($filter['smart'])) {
+                        if(!preg_match('/\W(?:S(\d+)E(\d+)|(\d+)x(\d+)(?:\.(\d+))?)\W/', $item['title'], $match))
+                            continue;
+                        
+                        $id = implode('x', array_slice($match, 3));
+                        if (is_array($filter['episodes']) && is_array($filter['episodes']['rule'])) {
+                            if (in_array($id, $filter['episodes']['rule'])) {
+                                rss_log("Already have episode $id", VERBOSE_EXTRA);
+                                add_item(&$feed, $item);
+                                continue 2;
+                            }
+                            $filter['episodes']['rule'][] = $id;
+                        }
+                        else
+                            $filter['episodes'] = array('rule' => array($id));
+                            
+                        rss_log("New epidose $id", VERBOSE_EXTRA);
+                    }
+                    
+                    if (add_torrent(get_download($item), !empty($filter['directory']) ? $filter['directory'] : $feed['directory']) == 0) {
+                        $item['downloaded'] = true;
+                    }
+                    else rss_log("Unable to add {$item['title']} from " . get_download($item), VERBOSE_ERROR);
+                }
+            }
+        }
+        
+        add_item(&$feed, $item);
+    }   
+}
+
+rss_log('Saving data', VERBOSE_EXTRA);
+
+$History->write();
+write_config();
---
+++ branches/cookie/www/rss_filter_edit.php
@@ -0,0 +1,140 @@
+#!/usr/local/bin/php
+<?php
+require("guiconfig.inc");
+
+$id = $_GET['id'];
+if (isset($_POST['id']))
+    $id = $_POST['id'];
+
+$pgtitle = array(gettext('Extensions'), gettext("RSS"),gettext("Filter"),isset($id)?gettext("Edit"):gettext("Add"));
+
+if (!is_array($config['rss']['filters'])) $config['rss']['filters'] = array('rule'=>array());
+array_sort_key($config['rss']['filters']['rule'], "name");
+
+$a_filter = &$config['rss']['filters']['rule'];
+$a_feed = &$config['rss']['feeds']['rule'];
+
+if (isset($id) && $a_filter[$id]) {
+    $pconfig['uuid'] = $a_filter[$id]['uuid'];
+    $pconfig['name'] = $a_filter[$id]['name'];
+    $pconfig['filter'] = $a_filter[$id]['filter'];
+    $pconfig['directory'] = $a_filter[$id]['directory'];
+    $pconfig['enabled'] = $a_filter[$id]['enabled'];
+    $pconfig['feed'] = $a_filter[$id]['feed'];
+    $pconfig['smart'] = $a_filter[$id]['smart'];
+    $pconfig['episodes'] = $a_filter[$id]['episodes'];
+} else {
+    $pconfig['uuid'] = uuid();
+    $pconfig['name'] = '';
+    $pconfig['filter'] = '';
+    $pconfig['directory'] = '';
+    $pconfig['enabled'] = false;
+    $pconfig['feed'] = -1;
+    $pconfig['episodes'] = array();
+}
+
+if ($_POST) {
+    unset($input_errors);
+    $pconfig = $_POST;
+
+    /* check for name conflicts */
+    foreach ($a_filter as $filter) {
+        if (isset($id) && ($a_filter[$id]) && ($a_filter[$id] === $filter))
+            continue;
+
+        if ($filter['name'] == $_POST['name']) {
+            $input_errors[] = gettext("This filter already exists in the filter list.");
+            break;
+        }
+    }
+    
+    $reqdfields = explode(" ", "name filter");
+    $reqdfieldsn = array(gettext("Name"), gettext("Filter"));
+    do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
+    
+    if (!empty($_POST['directory']) && !is_dir($_POST['directory'])) $input_errors[] = gettext("Download directory does not exist.");
+
+    if (!$input_errors) {
+        $filter = array();
+        $filter['uuid'] = $_POST['uuid'];
+        $filter['name'] = $_POST['name'];
+        $filter['filter'] = $_POST['filter'];
+        $filter['directory'] = $_POST['directory'];
+        $filter['enabled'] = $_POST['enabled'] ? true : false;
+        $filter['feed'] = $_POST['feed'];
+        $filter['smart'] = $_POST['smart'] ? true : false;
+        $filter['episodes'] = unserialize($_POST['episodes']);
+
+        if (isset($id) && $a_filter[$id]) {
+            $a_filter[$id] = $filter;
+        } else {
+            $a_filter[] = $filter;
+        }
+
+        write_config();
+
+        header("Location: extension_rss_filter_manage.php");
+        exit;
+    }
+}
+
+
+?>
+<?php include("fbegin.inc");?>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+        <td class="tabnavtbl">
+            <ul id="tabnav">
+                <li class="tabinact"><a href="extension_rss_feed_manage.php"><span><?=gettext("Feeds");?></span></a></li>
+                <li class="tabact"><a href="extension_rss_filter_manage.php" title="<?=gettext("Reload page");?>"><span><?=gettext("Filters");?></span></a></li>
+                <li class="tabinact"><a href="extension_rss_history.php"><span><?=gettext("History");?></span></a></li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td class="tabcont">
+            <form action="extension_rss_filter_edit.php" method="post" name="iform" id="iform">
+                <?php if ($input_errors) print_input_errors($input_errors); ?>
+                <table width="100%" border="0" cellpadding="6" cellspacing="0">
+                    <?php html_inputbox("name", gettext("Name"), $pconfig['name'], gettext("You may enter a name here for your reference."), true, 40);?>
+                    <?php html_inputbox("filter", gettext("Filter"), $pconfig['filter'], gettext("Enter a Perl regular expression."), true); ?>
+                    <?php html_filechooser("directory", gettext("Download directory"), $pconfig['directory'], gettext("Where to save downloaded data."), $g['media_path'], true, 60); ?>
+                    <tr>
+                        <td width="22%" valign="top" class="vncell"><?=gettext("Enabled");?></td>
+                        <td width="78%" class="vtable">
+                            <input name="enabled" type="checkbox" id="enabled" value="yes" <?php if (isset($pconfig['enabled'])) echo "checked";?>>
+                            <span class="vexpl"><?=gettext("Enable this filter.");?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="22%" valign="top" class="vncell"><?=gettext("Smart Filter");?></td>
+                        <td width="78%" class="vtable">
+                            <input name="smart" type="checkbox" id="smart" value="yes" <?php if (isset($pconfig['smart'])) echo "checked";?>>
+                            <span class="vexpl"><?=gettext("Attempt to filter series episodes.");?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="22%" valign="top" class="vncell"><?=gettext("Feed");?></td>
+                        <td width="78%" class="vtable">
+                            <select name="feed" class="formfld" id="feed">
+                                <option value="-1" <?if (-1 == $pconfig['feed']) echo 'selected';?>></option>
+                                <?php foreach ($a_feed as $feedk => $feedv): ?>
+                                <option value="<?=$feedv['uuid'];?>" <?php if ($feedv['uuid'] == $pconfig['feed']) echo "selected";?>><?php echo htmlspecialchars($feedv['name']);?></option>
+                                <?php endforeach; ?>
+                            </select>
+                      </td>
+                    </tr>
+                </table>
+                <div id="submit">
+                    <input name="Submit" type="submit" class="formbtn" value="<?=((isset($id) && $a_filter[$id]))?gettext("Save"):gettext("Add")?>">
+                    <input name="uuid" type="hidden" value="<?=$pconfig['uuid'];?>">
+                    <input name="episodes" type="hidden" value="<?=htmlspecialchars(serialize($pconfig['episodes']));?>">
+                    <?php if (isset($id) && $a_filter[$id]): ?>
+                    <input name="id" type="hidden" value="<?=$id;?>">
+                    <?php endif; ?>
+                </div>
+            </form>
+        </td>
+    </tr>
+</table>
+<?php include("fend.inc");?>
