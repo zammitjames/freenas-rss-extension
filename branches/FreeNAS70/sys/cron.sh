813f6506b301f595c5d92021b2d0613b9383f6c3
tresni <tresni@d4fb0142-069c-11de-ba4d-252e5d5a8945>
2009-11-30 07:18:24 +0000

    Pruning obselete branch FreeNAS70

--- branches/FreeNAS70/sys/cron.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-SCRIPT_NAME=rss_cron.php
-# for /bin/bash in case /bin/sh ever disappears
-#SCRIPT_FILENAME="${0/cron.sh/$SCRIPT_NAME}"
-SCRIPT_FILENAME="${0%/*}/$SCRIPT_NAME"
-/usr/local/bin/php -f "$SCRIPT_FILENAME"
